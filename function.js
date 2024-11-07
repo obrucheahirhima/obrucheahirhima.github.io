880b4569f4a71464c490af73ca342bf83ce00079
obrucheahirhima <105253330+obrucheahirhima@users.noreply.github.com>
2024-11-07 21:24:13 +0200

    Update function.js

--- function.js
+++ function.js
@@ -1,3 +1,13 @@
+function toggleNav() {
+  var navbar = document.getElementById("navbar");
+  if (navbar.style.display === "block") {
+    navbar.style.display = "none";
+  } else {
+    navbar.style.display = "block";
+  }
+}
+
+
 document.getElementById('subscribe-form').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent form submission
     document.getElementById('thank-you-message').style.display = 'block'; // Show thank you message
